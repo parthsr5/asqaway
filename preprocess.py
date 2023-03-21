c477855bbd0841a3a6477de633979c534cdc1d94
Jason Rauchwerk <jrauchwerk@gmail.com>
2023-03-21 12:06:37 -0400

    Preprocessing only uses AA and builds before replacing

--- preprocess.py
+++ preprocess.py
@@ -11,51 +11,95 @@ email = os.environ['EMAIL']
 apikey = os.environ['API_KEY']
 inst = Submission(email, apikey)
 
-def preprocess_list(datum):
+
+def get_AA(text):
+    inst.init_mm_interactive(text)
+    response = inst.submit()
+    mappings = {}
+    if response.status_code == 200:
+        content = response.content.decode().split("\n")
+        for line in content[1:]:
+            m = line.split("|")
+            if len(m) > 1 and m[1] == "AA":
+                offset, length = tuple(int(x) for x in m[8].split(":"))
+                word = text[offset: offset + length]
+                if word not in mappings:
+                    mappings[word] = m[3]
+    else:
+        raise NotImplementedError
+    
+    return mappings
+
+def load_AA_list(datum):
+    AA = {}
+    AA.update(get_AA(datum['body']))
+    for answer in datum['exact_answer']:
+        AA.update(get_AA(answer[0]))
+    for snippet in datum['snippets']:
+        AA.update(get_AA(snippet))
+    return AA
+
+def load_AA_factoid(datum):
+    AA = {}
+    AA.update(get_AA(datum['body']))
+    AA.update(get_AA(datum['exact_answer'][0]))
+    for snippet in datum['snippets']:
+        AA.update(get_AA(snippet))
+    return AA
+
+def load_AA(dataset):
+    AA = {}
+    for datum in dataset:
+        if datum['type'] == 'list':
+            AA.update(load_AA_list(datum))
+        elif datum['type'] == 'factoid':
+            AA.update(load_AA_factoid(datum))
+    return AA
+
+
+def canonize(text, AA):
+    for word, new_word in AA.items():
+        text = re.sub(rf"\b{word}\b", new_word, text)
+    return text
+
+def preprocess_list(datum, AA):
     processed = []
     i = 1
     qid = datum['id']
 
-    # for answer in datum['exact_answer']:
-    #   answer = answer[0]
-    #   for snippet in datum['snippets']:
-    #     snippet = snippet['text']
-    datum['body'] = "List signaling molecules (ligands) that interact with the receptor (EGFR)"#datum['body'].replace('?', "")
-    print(canonize(datum['body']))
-    
-    for snippet in datum['snippets'][1:]:
-        snippet = snippet['text']
-        print(canonize(snippet))
-        exit()
-        for answer in datum['exact_answer']:
-            answer = answer[0].lower()
+    question = canonize(datum['body'], AA)
 
+    answers = [canonize(a[0], AA) for a in datum['exact_answer']]
+    
+    for snippet in datum['snippets']:
+        snippet = canonize(snippet['text'], AA)
+        for answer in answers:
             ind = snippet.find(answer)
             if ind > -1:
-                new_entry = {'id': f'{qid}_{i:04}', 'question': datum['body'], 'answers': [
+                new_entry = {'id': f'{qid}_{i:04}', 'question': question, 'answers': [
                     {'text': answer, 'answer_start': ind}]}
                 processed.append({'qas': [new_entry], 'context': snippet})
                 i += 1
     return processed
 
-
-def preprocess_factoid(datum):
+def preprocess_factoid(datum, AA):
     processed = []
     i = 1
     qid = datum['id']
 
+    question = canonize(datum['body'])
+
     for snippet in datum['snippets']:
-        snippet = snippet['text']
-        answer = datum['exact_answer'][0]
+        snippet = canonize(snippet['text'], AA)
+        answer = canonize(datum['exact_answer'][0], AA)
 
         ind = snippet.find(answer)
         if ind > -1:
-            new_entry = {'id': f'{qid}_{i:04}', 'question': datum['body'], 'answers': [
+            new_entry = {'id': f'{qid}_{i:04}', 'question': question, 'answers': [
                 {'text': answer, 'answer_start': ind}]}
             processed.append({'qas': [new_entry], 'context': snippet})
             i += 1
     return processed
-
 
 def preprocess_dataset(dataset):
     processed = []
@@ -66,41 +110,13 @@ def preprocess_dataset(dataset):
             processed.extend(preprocess_factoid(datum))
     return processed
 
-def canonize(text):
-    print(text)
-    inst.init_mm_interactive(text)
-    response = inst.submit()
-    mappings = {}
-    if response.status_code == 200:
-        content = response.content.decode().split("\n")
-        for line in content[1:]:
-            # print(line)
-            m = line.split("|")
-            if len(m) > 1:
-                # if m[1] == "MMI":
-                #     for expansion in m[8].split(","):
-                #         offset, length = tuple(int(x) for x in expansion.split("/"))
-                #         word = text[offset: offset + length]
-                #         if word not in mappings:
-                #             mappings[word] = m[3]
-                if m[1] == "AA":
-                    offset, length = tuple(int(x) for x in m[8].split(":"))
-                    word = text[offset: offset + length]
-                    if word not in mappings:
-                        mappings[word] = m[3]
-    else:
-        raise NotImplementedError
-
-    print(mappings)
-    for word, new_word in mappings.items():
-        text = text.replace(word, new_word)
-    return text
 
 if __name__ == "__main__":
     infile = "BioASQ-training11b/training11b.json"
     x = json.load(open(infile))
 
-    processed = preprocess_dataset(x['questions'])
+    AA = load_AA(x['questions'])
+    processed = preprocess_dataset(x['questions'], AA)
     out = {}
     out['data'] = []
     for i in x['data'][0]['paragraphs']:
